aa6ce4bc631200e9f1d5401e6845ee4688696fa7
tahsin52 <tahsin_c@hotmail.com>
2022-01-27 12:55:00 +0300

    product listing and index

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+function Footer() {
+    return (
+        <footer>
+            <Container>
+                <Row>
+                    <Col className="text-center">Copyright &copy; ProShop</Col>
+                </Row>
+            </Container>
+        </footer>
+
+    )
+}
+
+export default Footer
