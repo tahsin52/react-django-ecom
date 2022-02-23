5a8ab62f2ce28e343bba9718725b2ed4e164f5ea
tahsin52 <tahsin_c@hotmail.com>
2022-02-23 14:58:01 +0300

    --frontend user login reducer and action

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,38 @@
+import { USER_LOGIN_REQUEST, USER_LOGOUT, USER_LOGIN_FAIL, USER_LOGIN_SUCCESS } from "../constants/userConstants";
+import {PRODUCT_DETAILS_FAIL} from "../constants/productConstants";
+import axios from "axios";
+
+export const login = (email, password) => async (dispatch) => {
+    try{
+        dispatch({
+            type: USER_LOGIN_REQUEST
+        })
+
+        const config = {
+            headers:{
+                'Content-type': 'application/json'
+            }
+        }
+
+        const {data} = await  axios.post(
+            '/users/login/',
+            {'username': email, 'password': password},
+            config
+        )
+
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data
+        })
+
+        localStorage.setItem('userInfo', JSON.stringify(data))
+
+    }catch (error) {
+        dispatch({
+            type: USER_LOGIN_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message,
+        })
+    }
+}
