f4904b15d9b89911746a4b9655f4e489a940ca29
tahsin52 <tahsin_c@hotmail.com>
2023-01-14 19:06:37 +0300

    fixed

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -88,7 +88,7 @@ function CartScreen({ match, location, history }) {
                     <ListGroup variant='flush'>
                         <ListGroup.Item>
                             <h2>SubTotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)}) items</h2>
-                            ${cartItems.reduce((acc,item) => acc + item.qty * item.price, 0)}
+                            ${cartItems.reduce((acc,item) => acc + item.qty * item.price, 0).toFixed(3)}
                         </ListGroup.Item>
                     </ListGroup>
 
